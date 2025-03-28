849fbfbaaad28a26e882d84240459dbb5521abf0
matt <matthewquinones42@gmail.com>
2025-03-27 23:55:11 -0600

    - made duration and intensity flags optional - added default behavior for fork stress to run and a set intensity of no flag is entered for 10 seconds - set cpu stress defualt to 4 threads and 10s when not provided - added the optional default behavior to cpu, mem, and disk

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ struct Args {
     test_type: String,
 
     /// The intensity (number of threads) for the CPU stress test OR max processes for fork stress
-    #[arg(short, long, default_value_t = 4)]
-    intensity: usize,
+    #[arg(short, long)]
+    intensity: Option<usize>,
 
     /// Duration of the stress test in seconds (use '0' for indefinite)
-    #[arg(short, long, default_value_t = 10)]
-    duration: u64,
+    #[arg(short, long)]
+    duration: Option<u64>,
 
     /// The load percentage for CPU stress test (1-100%)
     #[arg(short, long)]
@@ -43,32 +43,39 @@ fn main() {
         "cpu" => {
 
             if args.fork {
-                println!("Running fork stress test... ");
-                fork_stress::stress_fork();
+                let max_procs = num_cpus::get() * 50; // 50x logical cores
+                let intensity = args.intensity.unwrap_or(max_procs); // max processes
+                let duration = args.duration.unwrap_or(10);           // 10 seconds default
+                println!("Starting fork stress test with a maximum of {} processes for {} seconds...", intensity, duration);
+                fork_stress::stress_fork(intensity, duration);
             }
 
             else
             {
             
+                let intensity = args.intensity.unwrap_or(4); // default CPU threads
+                let duration = args.duration.unwrap_or(10);  // default duration
                 let load = args.load.unwrap_or(100.0);
-                let indefinite = args.duration == 0;
+                let indefinite = duration == 0;
 
                 println!("Starting CPU stress test with {} threads at {}% load for {} seconds...",
-                    args.intensity, load, args.duration
+                    intensity, load, duration
                 );
 
-                cpu_stress::stress_cpu(args.intensity, load, args.duration, args.load.is_some(), indefinite);
+                cpu_stress::stress_cpu(intensity, load, duration, args.load.is_some(), indefinite);
             }
         }
         "mem" => {
-            println!("Starting memory stress test with {} MB for {} seconds...", args.size, args.duration);
+            let duration = args.duration.unwrap_or(10);
+            println!("Starting memory stress test with {} MB for {} seconds...", args.size, duration);
             memory_stress::check_memory_usage();
-            memory_stress::stress_memory(args.size, args.duration);
+            memory_stress::stress_memory(args.size, duration);
             memory_stress::check_memory_usage();
         }
         "disk" => {
-            println!("Starting disk stress test with {} MB for {} seconds...", args.size, args.duration);
-            disk_stress::stress_disk(args.size, args.duration);       
+            let duration = args.duration.unwrap_or(10);
+            println!("Starting disk stress test with {} MB for {} seconds...", args.size, duration);
+            disk_stress::stress_disk(args.size, duration);       
         }
         
         _ => {
